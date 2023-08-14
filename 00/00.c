a118074b29f963669bbe693797d99414d3ebe7b7
xyco <random@gmail.com>
2023-08-14 00:31:39 -0400

    pushed files

---
+++ 00/00.c
@@ -0,0 +1,15 @@
+// 
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    int bank[16];
+
+    printf("Welcome to bank 1337! Give us your money: \n");
+    gets(bank);
+    fflush(stdout);
+
+}
---
+++ 01/01.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+const char* hacked()
+{
+    const char* local_flag = {"XYC{H0wD1d1G3tH3r3?!}"};
+
+    printf("This is a secret function.\n");
+    printf("Access level 0 is required. All attempts to access will be logged.\n");
+    printf("Oh yeah, here's your flag: %s\n", local_flag);
+
+    return local_flag;
+
+}
+
+char * flag = "XYC{H0wD1d1G3tH3r3?!}";
+
+int main()
+{
+
+    char code[16];
+    int access = 0;
+
+    printf("Please enter the secret code: \n");
+    gets(code);
+
+    if(strcmp(code, "1337") == 0)
+    {
+        flag = hacked();
+
+        // hacked();
+
+        access = 1;
+    }
+
+    else
+    {
+        printf("Access denied, silent alarm has been triggered!\n");
+    }
+
+    if(access)
+    {
+    printf("Authentication successful as root (access level=%d) \n", access);
+    printf("You are root, granting access... \nAccess granted, welcome root.\n");
+    printf("Wow, you realized we could overflow the buffer and you could still get the flag? Nice.\nHere's your flag: %s", flag);
+    }
+
+    else
+    {
+        printf("Authentication failed as root (access level=%d)\n", access);
+    }
+
+    return 0;
+
+}
